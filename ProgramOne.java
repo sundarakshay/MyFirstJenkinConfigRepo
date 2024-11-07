ba702e0fcd37f58c50c2eec959a1114346cb2750
sundarakshay@123 <sundarakshay90@gmail.com>
2024-11-07 13:08:39 -0600

    First

---
+++ ProgramOne.java
@@ -0,0 +1,17 @@
+package javaexamples;
+
+public class ProgramOne {
+
+	public static void main(String[] args) {
+		System.out.println("Hellow");
+		int a = 5;
+		int b = 3;
+		int c = a+b;
+		float pi = 3.4f;
+		String name = "akshay";
+		System.out.println(c);
+		System.out.println(pi);
+		System.out.println(name);
+	}
+
+}
